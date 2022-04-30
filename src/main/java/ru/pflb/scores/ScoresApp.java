072bec84ac80ed3065d01c56b31ba7673c8a011d
Ye Golovnya <egolovnya@voximplant.com>
2022-04-30 14:42:06 +0300

    rm non-spring http routing

--- src/main/java/ru/pflb/scores/ScoresApp.java
+++ src/main/java/ru/pflb/scores/ScoresApp.java
@@ -9,24 +9,4 @@ public class ScoresApp {
     public static void main(String[] args) {
         SpringApplication.run(ScoresApp.class, args);
     }
-
-//
-//    public static void main(String[] args) {
-//        var sc = new ServerConfiguration(8080, routingHandler(), threadPoolExecutor());
-//        sc.start();
-//    }
-//
-//    private static Executor threadPoolExecutor() {
-//        return new ThreadPoolExecutor(10, 30, 120L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1200));
-//    }
-//
-//    private static RoutingHandler routingHandler() {
-//        var sessionHolder = new SessionEvictingHolder();
-//        var loginService = new NoAuthLoginService(sessionHolder);
-//        var loginHandler = new LoginHandler(loginService);
-//        var scoreService = new InMemoryScoreService(loginService);
-//        var scoresRecordHandler = new ScoresRecordHandler(scoreService);
-//        var highScoresHandler = new HighScoresHandler(scoreService);
-//        return new RoutingHandler(loginHandler, scoresRecordHandler, highScoresHandler);
-//    }
 }
