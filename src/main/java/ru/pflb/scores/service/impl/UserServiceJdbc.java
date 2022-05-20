1869b829c83bf2d680958918b86f8b853ff93c14
Ye Golovnya <egolovnya@voximplant.com>
2022-05-20 23:35:42 +0300

    TransactionTemplate

--- src/main/java/ru/pflb/scores/service/impl/UserServiceJdbc.java
+++ src/main/java/ru/pflb/scores/service/impl/UserServiceJdbc.java
@@ -3,6 +3,7 @@ package ru.pflb.scores.service.impl;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
 import ru.pflb.scores.entity.User;
 import ru.pflb.scores.service.UserService;
 
@@ -13,15 +14,21 @@ import java.sql.Types;
 public class UserServiceJdbc implements UserService {
 
     private final JdbcTemplate jdbcTemplate;
+    private final TransactionTemplate transactionTemplate;
 
     @Override
     public long registerUser(String name) {
+        return transactionTemplate.execute(status -> registerUserLocking(name));
+    }
+
+    private long registerUserLocking(String name) {
+        jdbcTemplate.execute("LOCK TABLE scores.user IN ROW EXCLUSIVE MODE");
+
         long id = generateId(); // ой, но что если кто-то теперь успеет обновить БД раньше нас
 
         jdbcTemplate.update(
                 "INSERT INTO scores.user (id, name) VALUES (?, ?)", id, name
         );
-
         return id;
     }
 
